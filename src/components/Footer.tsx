1f84a32c2ebfc668f829cb12c0a1741c0496b8ff
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-03-23 17:12:57 +1100

    Increase footer size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,12 @@
 import { Affix, Center, Container, Text } from "@mantine/core";
 
 const Footer = () => (
-  <Affix position={{ bottom: 5, right: "50%" }}>
-    <Text
-      color="gray"
-      size="xs"
-      align="center"
-      className="translate-x-1/2"
-      inline
-    >
-      The r/Apple Discord is in no way affiliated with Apple Inc.
-    </Text>
+  <Affix position={{ bottom: 5 }}>
+    <footer className="w-screen">
+      <Text color="gray" size="xs" align="center" inline>
+        The r/Apple Discord is in no way affiliated with Apple Inc.
+      </Text>
+    </footer>
   </Affix>
 );
 
